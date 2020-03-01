bdaf7f7e2ec2fb5042631b37b6bf75ad6c07f9f1
wataru40101 <59873250+wataru40101@users.noreply.github.com>
2020-03-01 13:29:05 +0900

    Add files via upload

---
+++ vegas.js
@@ -0,0 +1,8 @@
+$('.top-img').vegas({
+    slides: [
+        { src: './image/top1.jpg' },
+        { src: './image/top2.jpg' },
+        { src: './image/top3.jpg' }
+    ],
+    animation: 'kenburns'
+});
